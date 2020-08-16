42e6c032eb9f5f783c212d3259f70a64b57d4b3b
HemaSundar Tatipudi <49449270+hemasundar-tatipudi@users.noreply.github.com>
2020-08-16 17:23:22 +0530

    Assignment

---
+++ Day 3/Sorted Infinite array.py
@@ -0,0 +1,27 @@
+def binary_search(l,key,beg,end):
+    
+    while beg<=end:
+        mid = (beg+end)//2
+        if key==l[mid]:
+            return mid
+        elif key<l[mid]:
+            end=mid-1
+        else:
+            beg=mid+1
+            
+    return -1
+
+
+def search(l,key,beg,end):
+    if key<=l[end]:
+        return binary_search(l,key,beg,end)
+    else:
+        beg = end
+        end *= 2
+        return search(l,key,beg,end)
+        
+
+l = list(map(int,input().strip().split(',')))
+key = int(input())
+
+print(search(l,key,0,1))
